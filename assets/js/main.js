58009f4912fc536508a81e663289d7bc68a3db7b
Frank <yoinspiration@outlook.com>
2024-02-21 16:08:36 +0800

    优化搜索提示框

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,9 @@ const searchTerm = document.querySelector("#search-term");
 const searchHint = document.querySelector("#search-hint");
 
 searchTerm.addEventListener("input", () => {
-  searchHint.textContent = searchTerm.value;
+  if (searchTerm.value.length !== 0) {
+    searchHint.style.display = "block";
+  } else {
+    searchHint.style.display = "none";
+  }
 });
